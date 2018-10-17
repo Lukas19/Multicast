d7a2fecbc51ee16fcd7d4b8f91b33b40c8575c57
Lukas <lukas.zamora.14@sansano.usm.cl>
2018-10-17 15:22:46 -0300

    Socket Threads

--- src/serverpackage/Client.java
+++ src/serverpackage/Client.java
@@ -1,41 +1,46 @@
 package serverpackage;
 
+import com.sun.deploy.trace.SocketTraceListener;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.UnknownHostException;
+import java.util.concurrent.locks.StampedLock;
 
 public class Client {
 
-    final static int PORT = 8888;
+    final static int PRESSUREPORT = 10001;
+    final static int TEMPERATUREPORT = 10002;
+    final static int HUMIDITYPORT = 10003;
+
+
 
     public static void main(String[] args) throws UnknownHostException {
         String ipMulticast = args[0];
+        String measurementBinary = args[1];
+
         // Get the address that we are going to connect to.
-        InetAddress address = InetAddress.getByName(args[0]);
+        InetAddress address = InetAddress.getByName(ipMulticast);
 
-        // Create a buffer of bytes, which will be used to store
-        // the incoming bytes containing the information from the server.
-        // Since the message is small here, 256 bytes should be enough.
-        byte[] buf = new byte[256];
-
-        // Create a new Multicast socket (that will allow other sockets/programs
-        // to join it as well.
-        try (MulticastSocket clientSocket = new MulticastSocket(PORT)){
-            //Joint the Multicast group.
-            clientSocket.joinGroup(address);
-
-            while (true) {
-                // Receive the information and print it.
-                DatagramPacket msgPacket = new DatagramPacket(buf, buf.length);
-                clientSocket.receive(msgPacket);
-
-                String msg = new String(buf, 0, buf.length);
-                System.out.println("Socket 1 received msg: " + msg);
+        //Review measurement
+        for (int i = 0; i < measurementBinary.length(); i++){
+            if (i == 0 && measurementBinary.charAt(i) == 1){
+                Runnable r1 = new ThreadClient(address, PRESSUREPORT);
+                Thread t1 = new Thread(r1);
+                t1.start();
             }
-        } catch (IOException ex) {
-            ex.printStackTrace();
+            else if (i == 1 && measurementBinary.charAt(i) == 1){
+                Runnable r2 = new ThreadClient(address, TEMPERATUREPORT);
+                Thread t2 = new Thread(r2);
+                t2.start();
+            }
+            else if (i == 2 && measurementBinary.charAt(i) == 1){
+                Runnable r3 = new ThreadClient(address, HUMIDITYPORT);
+                Thread t3 = new Thread(r3);
+                t3.start();
+            }
         }
     }
 }
---
+++ src/serverpackage/ThreadClient.java
@@ -0,0 +1,47 @@
+package serverpackage;
+import com.sun.deploy.trace.SocketTraceListener;
+import java.io.IOException;
+import java.lang.*;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+import java.util.concurrent.locks.StampedLock;
+
+
+
+
+public class ThreadClient implements Runnable{
+    private InetAddress ip;
+    private int port;
+
+    public ThreadClient(InetAddress ipMulticast, int selectedPort){
+        ip = ipMulticast;
+        port = selectedPort;
+    }
+
+    public void run () {
+        // Create a buffer of bytes, which will be used to store
+        // the incoming bytes containing the information from the server.
+        // Since the message is small here, 256 bytes should be enough.
+        byte[] buf = new byte[256];
+        // Create a new Multicast socket (that will allow other sockets/programs
+        // to join it as well.
+        try (MulticastSocket clientSocket = new MulticastSocket(port)){
+            //Joint the Multicast group.
+            clientSocket.joinGroup(ip);
+
+            while (true) {
+                // Receive the information and print it.
+                DatagramPacket msgPacket = new DatagramPacket(buf, buf.length);
+                clientSocket.receive(msgPacket);
+
+                String msg = new String(buf, 0, buf.length);
+                System.out.println("Socket 1 received msg: " + msg);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
